db493f3848e331256a498f13c5aff468165f75d2
gowthamgowtham50799@gmail.com <gowthamgowtham50799@gmail.com>
2024-12-28 16:06:49 +0530

    employee

---
+++ src/date/date.java
@@ -0,0 +1,15 @@
+package date;
+
+public class date {
+
+	private void name() {
+		System.out.println("company name : TCS");
+	}
+	private void Location() {
+		System.out.println("Company Location : Chennai");
+	}
+	public static void main(String[] args) {
+		date c=new date();
+		c.name();
+		c.Location();
+	}}
